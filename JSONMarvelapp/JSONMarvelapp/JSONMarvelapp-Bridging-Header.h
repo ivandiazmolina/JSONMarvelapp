af709ca61c96876523c49b603af4837b06c544e1
Iván Díaz Molina <ivandiazmolina92@gmail.com>
2020-12-01 16:03:45 +0100

    added IDIAZM library

---
+++ JSONMarvelapp/JSONMarvelapp/JSONMarvelapp-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  JSONMarvelapp-Bridging-Header.h
+//  JSONMarvelapp
+//
+//  Created by Iván Díaz Molina on 1/12/20.
+//  Copyright © 2020 IDIAZM. All rights reserved.
+//
+
+#import <IDIAZM/IDIAZM-Swift.h>
