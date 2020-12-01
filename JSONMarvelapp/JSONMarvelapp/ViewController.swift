d09252785f81513f63fc8ab5409ed13d72cacbbe
Iván Díaz Molina <ivandiazmolina92@gmail.com>
2020-12-01 15:55:44 +0100

    Initial commit

---
+++ JSONMarvelapp/JSONMarvelapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONMarvelapp
+//
+//  Created by Iván Díaz Molina on 1/12/20.
+//  Copyright © 2020 IDIAZM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
